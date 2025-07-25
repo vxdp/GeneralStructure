188392370ce33fc2eb9ce6cae9199e66c373eb86
vxdp <vxdpplace@gmail.com>
2025-07-25 02:53:03 +0200

    v 0.2

---
+++ src/server/Core/test.server.lua
@@ -0,0 +1,4 @@
+local Common = require(game.ReplicatedStorage.Common.common_init_locked)
+local RojoFriendlyEvents = Common.Lib.RojoFriendlyEvents
+
+local Event = RojoFriendlyEvents.InitializeOnServer("EventTest1", "RemoteEvent", game.ReplicatedStorage.Remotes.Reliable)
---
+++ src/shared/Common/Lib/EventRateLimiter/readme.lua
@@ -0,0 +1,7 @@
+--Used in:
+--RojoFriendlyEvents 
+
+--[[
+    This is a simple rate limiting script.
+    Before firing an event, it checks if it has already been fired recently, if so, then it blocks the signal.
+]]
